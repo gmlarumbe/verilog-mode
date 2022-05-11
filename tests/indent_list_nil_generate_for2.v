83f8b9540760bd8103bc19902beb44289b1f1973
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2022-05-11 21:18:17 +0200

    Fix indentation if verilog-indent-lists is nil

---
+++ tests/indent_list_nil_generate_for2.v
@@ -0,0 +1,20 @@
+module foo (
+input wire a,
+input wire b,
+output reg z
+);
+endmodule
+
+localparam CONST=1;
+
+generate
+for (genvar i=0; i<CONST; i++)
+inst inst(.a(a[i]), .b(b[i]), .z(z[i]));
+endgenerate
+
+endmodule
+
+
+// Local Variables:
+// verilog-indent-lists: nil
+// End:
---
+++ tests_ok/indent_list_nil_generate_for.v
@@ -0,0 +1,25 @@
+module foo (
+   input wire a,
+   input wire b,
+   output reg z
+);
+endmodule
+
+localparam CONST=1;
+
+generate
+   for (genvar i=0; i<CONST; i++) begin : label
+      inst inst(
+         .a(a[i]),
+         .b(b[i]),
+         .z(z[i])
+      );
+   end : label
+endgenerate
+
+endmodule
+
+
+// Local Variables:
+// verilog-indent-lists: nil
+// End:
