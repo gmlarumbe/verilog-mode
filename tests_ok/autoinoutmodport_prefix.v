baa57057f4524f5969f713d25a8a5029d9aab416
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2022-07-24 17:55:30 +0200

    Add support to align expressions of continuous assignments.
    * verilog-mode.el (Line#769, verilog-align-assign-expr,
    verilog-basic-complete-no-assign-re, verilog-complete-no-assign-re,
    verilog-mode, verilog-pretty-expr, verilog-submit-bug-report):
    
    Add support to align expressions of continuous assignments.

--- tests_ok/autoinoutmodport_prefix.v
+++ tests_ok/autoinoutmodport_prefix.v
@@ -36,17 +36,17 @@ module ExampMain
    ExampleIf ib;
    
    // Manually declared (duplications are NOT detected yet)
-   assign a_out = 1;
+   assign a_out      = 1;
    
    /*AUTOASSIGNMODPORT("ExampIf", "mp", "ia", "", "a_")*/
    // Beginning of automatic assignments from modport
-   assign a_out = ia.out;
+   assign a_out      = ia.out;
    assign ia.req_dat = a_req_dat;
    assign ia.req_val = a_req_val;
    // End of automatics
    /*AUTOASSIGNMODPORT("ExampIf", "mp", "ib", "", "b_")*/
    // Beginning of automatic assignments from modport
-   assign b_out = ib.out;
+   assign b_out      = ib.out;
    assign ib.req_dat = b_req_dat;
    assign ib.req_val = b_req_val;
    // End of automatics
